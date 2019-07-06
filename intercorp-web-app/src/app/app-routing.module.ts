095781af71569cfe29c583c5289a614d72760a72
JeniferCoila <jeniferfcr@gmail.com>
2019-07-06 16:55:11 -0500

    Setup del proyecto Angular

---
+++ intercorp-web-app/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [NgbModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
