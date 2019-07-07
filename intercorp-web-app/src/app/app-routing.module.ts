0c55fd634cc7118b4c42fd6fb0aa70d869be2f34
JeniferCoila <jeniferfcr@gmail.com>
2019-07-07 11:37:02 -0500

    Creación de formulario y y variables

--- intercorp-web-app/src/app/app-routing.module.ts
+++ intercorp-web-app/src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './home/form/form.component';
+import { ClientListComponent } from './home/client-list/client-list.component';
+import { AnalyticsComponent } from './home/analytics/analytics.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent,
+    children:[
+      {path: 'form', component: FormComponent},
+      {path: 'list', component: ClientListComponent},
+      {path: 'analytics', component:AnalyticsComponent}
+    ]
+    
+},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [NgbModule, RouterModule.forRoot(routes)],
--- intercorp-web-app/src/app/app.module.ts
+++ intercorp-web-app/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FormComponent } from './home/form/form.component';
+import { ClientListComponent } from './home/client-list/client-list.component';
+import { AnalyticsComponent } from './home/analytics/analytics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    FormComponent,
+    ClientListComponent,
+    AnalyticsComponent
   ],
   imports: [
     BrowserModule,
